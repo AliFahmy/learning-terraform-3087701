bafbf763336c7d4ae852c00a3b94b9cb559ca862
Ali Khalifa <ali.khalifa@rackspace.com>
2024-11-13 14:48:45 +0200

    added alb listener resource

--- main.tf
+++ main.tf
@@ -59,16 +59,20 @@ module "alb" {
     }
   }
 
-  listeners = {
-    http ={
-      port = 80
-      protocol= "HTTP"
-    }
-  }
-
   tags = {
     Environment = "dev"
   }
+}
+
+resource "aws_lb_listener" "blog_alb_listener" {
+ load_balancer_arn = module.alb.arn
+ port              = "80"
+ protocol          = "HTTP"
+
+ default_action {
+   type             = "forward"
+   target_group_arn = module.alb.target_groups[0]
+ }
 }
 
 
