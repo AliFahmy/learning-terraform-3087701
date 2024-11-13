ba58564cd43334b00a1a35309cd779bae5444339
Ali Khalifa <ali.khalifa@rackspace.com>
2024-11-13 14:59:48 +0200

    adjusted count referencing

--- main.tf
+++ main.tf
@@ -56,7 +56,7 @@ module "alb" {
       protocol         = "HTTP"
       port             = 80
       target_type      = "instance"
-      target_id        = aws_instance.blog.id
+      target_id        = aws_instance.blog[count.index].id
     }
   }
 
